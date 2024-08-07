dc1545e583af037562fb0584261a790fb0134d68
Drew <pantsrain@gmail.com>
2024-08-07 14:02:21 -0500

    nevermind we need to do this from a slightly different location

--- scripts/workshop-upload.sh
+++
@@ -1,4 +0,0 @@
-GMOD_ROOT="/q/Steam/steamapps/common/GarrysMod"
-
-"$GMOD_ROOT/bin/gmad.exe" create -folder . /tmp/zscontent.gma
-"$GMOD_ROOT/bin/gmpublish.exe" update -addon /tmp/zscontent.gma -id "1479340077"
