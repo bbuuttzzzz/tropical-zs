ef855852cf39a44c66efead544aeb4a2c976f66f
Drew <pantsrain@gmail.com>
2024-08-07 13:58:48 -0500

    add tools for updating the addon

---
+++ scripts/workshop-upload.sh
@@ -0,0 +1,4 @@
+GMOD_ROOT="/q/Steam/steamapps/common/GarrysMod"
+
+"$GMOD_ROOT/bin/gmad.exe" create -folder . /tmp/zscontent.gma
+"$GMOD_ROOT/bin/gmpublish.exe" update -addon /tmp/zscontent.gma -id "1479340077"
